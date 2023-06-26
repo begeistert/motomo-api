2f02b72ebd183547058788bcf6abdae06061eb42
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2023-06-26 00:29:30 -0600

    Redefine root

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/api', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
 
