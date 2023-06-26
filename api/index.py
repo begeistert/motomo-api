c8d524f6828b3b8aa074bed0f04ee182938bc06c
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2023-06-26 00:55:27 -0600

    Add the menu

--- api/index.py
+++ api/index.py
@@ -10,22 +10,84 @@ class handler(BaseHTTPRequestHandler):
         return
 """
 
-from flask import Flask, Blueprint
-
-bp = Blueprint('v1', __name__, template_folder='templates')
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.register_blueprint(bp, url_prefix='/api/v1')
+root = '/api/v1'
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route(f'{root}/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
 
 
-@app.route('/v1/', methods=['GET'])
+@app.route(f'{root}/menu', methods=['GET'])
+def menu():
+    return jsonify({
+        'Entries': {
+            1: {
+                'Name': 'Sopa Misa',
+                'Description': 'Nuestra mejor sopa'
+            },
+            2: {
+                'Name': 'Sushi de camarón',
+                'Description': 'Nuestro mejor sushi'
+            },
+            3: {
+                'Name': 'Pulpo asado',
+                'Description': 'Nuevo platillo'
+            },
+            4: {
+                'Name': 'Yakimeshi',
+                'Description': 'Con verduras'
+            }
+        },
+        'Main Dish':{
+            11: {
+                'Name': 'Ramen de cerdo ahumado',
+                'Description': 'Sabroso y ahumado'
+            },
+            12: {
+                'Name': 'Ramen vegano picante',
+                'Description': 'Especiado y reconfortante'
+            },
+            13: {
+                'Name': 'Ramen de pollo teriyaki',
+                'Description': 'Dulce y satisfactorio'
+            },
+            14: {
+                'Name': 'Ramen de mariscos',
+                'Description': 'Delicioso y abundante'
+            }
+        },
+        'Beverages': {
+            21: {
+                'Name': 'Café expresso',
+                'Description': 'Intenso y aromático'
+            },
+            22: {
+                'Name': 'Limonada refrescante',
+                'Description': 'Cítrica y revitalizante'
+            },
+            23: {
+                'Name': 'Agua de fresa',
+                'Description': 'Dulce y cremoso'
+            },
+            24: {
+                'Name': 'Té verde frío',
+                'Description': 'Refrescante y saludable'
+            }
+        }
+    })
+
+
+@app.route(f'{root}/categories', methods=['GET'])
 def categories():
-    pass
+    return jsonify([
+        'Entries',
+        'Main Dish',
+        'Beverages'
+    ])
 
 
 if __name__ == '__main__':
