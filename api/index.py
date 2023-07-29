d0168ac9f88047fdc780648b99a520692ad55638
Iván Montiel Cardona <ivan_montiel2013@hotmail.com>
2023-07-29 00:49:46 -0600

    Add urls to the food items

--- api/index.py
+++ api/index.py
@@ -11,6 +11,7 @@ data = {
             'dsc': 'Nuestra mejor sopa',
             'price': 95.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968271-9499c990-087a-4194-a29e-8de47104e01a.jpg'
         },
         {
             'id': 2,
@@ -18,6 +19,7 @@ data = {
             'dsc': 'Nuestro mejor sushi',
             'price': 85.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968386-691f8463-0b26-4009-acb1-3176945888ab.jpeg'
         },
         {
             'id': 3,
@@ -25,6 +27,7 @@ data = {
             'dsc': 'Nuevo platillo',
             'price': 90.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968426-93e012de-593c-4b69-8859-7859a8687f1b.jpeg'
         },
         {
             'id': 4,
@@ -32,6 +35,7 @@ data = {
             'dsc': 'Con verduras',
             'price': 110.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968453-7495bd2e-79ba-48b7-b4f2-bff9ce0fb1d2.jpeg'
         }
     ],
     'Main': [
@@ -41,6 +45,7 @@ data = {
             'dsc': 'Sabroso y ahumado',
             'price': 95.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968503-97b22881-ce57-4f54-a2d2-fae016abf440.jpg'
         },
         {
             'id': 12,
@@ -48,6 +53,7 @@ data = {
             'dsc': 'Especiado y reconfortante',
             'price': 85.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968531-8598e7a0-ceef-4b76-845c-02019735e877.jpg'
         },
         {
             'id': 13,
@@ -55,6 +61,7 @@ data = {
             'dsc': 'Dulce y satisfactorio',
             'price': 90.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968546-9918aea4-9ba8-4a0d-a758-930cd23c9c75.jpg'
         },
         {
             'id': 14,
@@ -62,6 +69,7 @@ data = {
             'dsc': 'Delicioso y abundante',
             'price': 110.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968559-8b669d90-baeb-4bd0-9968-ff6306daaf29.jpg'
         }
     ],
     'Beverages': [
@@ -71,6 +79,7 @@ data = {
             'dsc': 'Intenso y aromático',
             'price': 50.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968578-fe67cbfb-7dd7-4d74-b476-96d329ff75f4.jpeg'
         },
         {
             'id': 23,
@@ -78,6 +87,7 @@ data = {
             'dsc': 'Cítrica y revitalizante',
             'price': 60.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968596-17769368-77db-490e-9ff0-c904654c5949.jpeg'
         },
         {
             'id': 24,
@@ -85,6 +95,7 @@ data = {
             'dsc': 'Dulce y cremoso',
             'price': 90.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968607-d1903316-c19f-426b-a38f-82b6b0539089.jpg'
         },
         {
             'id': 25,
@@ -92,6 +103,7 @@ data = {
             'dsc': 'Refrescante y saludable',
             'price': 55.0,
             'img': 0,
+            'url': 'https://github-production-user-asset-6210df.s3.amazonaws.com/36460223/256968618-f9fc18ce-fd9d-476f-b29f-c7a6baf2128e.jpeg'
         }
     ]
 }
